609770aa2f05fbb20a1c4db4cccd2619b1b3118b
Beatriz-SMB <bsnbernardors@gmail.com>
2026-04-04 16:33:42 -0300

    feat(header): add sections to App component for content display

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ function App() {
   return (
     <>
       <Header />
+      <section id='sec-1' className='h-screen bg-blue'>
+        <div>Seção 1</div>
+      </section>
+      <section id='sec-2' className='h-screen bg-red'>
+        <div>Seção 2</div>  
+      </section>
+      <section id='sec-3' className='h-screen bg-green'>
+        <div>Seção 3</div>
+      </section>
     </>
   )
 }
