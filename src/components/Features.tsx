89cd0749010f507be56f1cd425ecdce129a3fb52
murillodominguez <murillo.v.carballo@gmail.com>
2026-04-06 10:40:41 -0300

    feat: add Features component

---
+++ src/components/Features.tsx
@@ -0,0 +1,12 @@
+function Features() {
+    return (
+        <div className="flex flex-col justify-center items-center mb-4">
+            <ul className="list-disc">
+                <li>App Mobile</li>
+                <li>Engenharia de Software de mobilidade</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Features;
