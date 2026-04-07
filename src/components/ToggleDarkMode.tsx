570d3e6421ebdcc3e76fd546aaaab44ddac41e03
Beatriz-SMB <bsnbernardors@gmail.com>
2026-04-07 17:07:51 -0300

    refactor: update ToggleDarkMode button for improved accessibility and visual clarity

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -1,11 +1,13 @@
 import useDarkMode from "@/hooks/useDarkMode";
+import { Moon, Sun } from 'lucide-react';
 
 function ToggleDarkMode() {
     const { isDarkMode, toggleDarkMode } = useDarkMode();
 
     return (
-        <button className="cursor-pointer p-2" onClick={toggleDarkMode}>
-            { isDarkMode ? "Dark mode" : "Light mode"}
+        <button className="cursor-pointer p-2 outline-none" onClick={toggleDarkMode}>
+            {/* { isDarkMode ? "Dark mode" : <Sun /> + "Light mode"} */}
+            { isDarkMode ? <Moon /> : <Sun /> }
         </button>
     )
 }
