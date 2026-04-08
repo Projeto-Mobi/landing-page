b4fa5cd2da25f33ae055cc9cbe053bbb3f992614
murillodominguez <murillo.v.carballo@gmail.com>
2026-04-08 02:27:30 -0300

    refactor: change Features background color and styles for dark mode

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,8 +2,8 @@ import { Smartphone, Car } from 'lucide-react';
 
 function Features() {
     return (
-        <section className="h-auto py-18 w-full flex flex-col justify-center items-center mb-4 bg-[#f9fafb]">
-            <ul className="list-disc flex flex-col gap-22 md:flex-row">
+        <section className="h-auto py-18 w-full flex flex-col justify-center items-center mb-4">
+            <ul className="list-disc dark:text-black flex flex-col gap-22 md:flex-row">
                 <li className='bg-white list-none w-48 py-12 px-4 border border-gray-200 rounded-lg flex flex-col gap-2 items-center shadow-sm hover:shadow-xl transform hover:scale-105 transition duration-300 ease-in-out'>
                     <Smartphone size={64} />
                     <span>App Mobile</span>
